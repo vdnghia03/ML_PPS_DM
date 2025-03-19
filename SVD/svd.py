fc914e71799ea4a2bd8ba2b5f60cafc550a76c5e
Duy Nghia <110990186+vdnghia03@users.noreply.github.com>
2025-03-19 10:43:33 +0700

    Update and rename bai1.py to svd.py

--- SVD/svd.py
+++ SVD/svd.py
@@ -18,3 +18,15 @@ def tinh_svd(ma_tran_A):
 
     ma_tran_Sigma = np.array(ma_tran_zero)
     return ma_tran_U, ma_tran_Sigma, ma_tran_Vt
+
+# Test HW 1
+ma_tran_A = np.array([[1, 0, 0, 0, 2], [0, 0, 3, 0, 0], [0, 0, 0, 0, 0], [0, 2, 0, 0, 0]])
+ma_tran_U, ma_tran_Sigma, ma_tran_V = tinh_svd(ma_tran_A)
+print("Ma tran U:")
+print(ma_tran_U)
+print("\nMa tran Sigma:")
+print(ma_tran_Sigma)
+print("\nMa tran V:")
+print(ma_tran_V)
+print("\n Nhan ba ma tran UZV: ")
+print(np.dot(np.dot(ma_tran_U, ma_tran_Sigma), ma_tran_V))
